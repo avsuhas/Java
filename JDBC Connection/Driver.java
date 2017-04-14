8631c70c24053e65b31dae1f4f88cc5f53286c26
Suhas V Arudi <avsuhas@gmail.com>
2017-04-13 23:31:48 -0400

    Update Driver.java

--- JDBC Connection/Driver.java
+++ JDBC Connection/Driver.java
@@ -7,12 +7,13 @@ public class Driver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try{
+			//1. get a connection to database
 			Connection myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root" , "suhas123");
-			
+			//2.Create a statement
 			Statement myStat = myConn.createStatement();
-			
+			//3.Execute SQL query
 			ResultSet myRs = myStat.executeQuery("select * from employees");
-			
+			//4. Process the result
 			while(myRs.next()){
 				System.out.println(myRs.getString("last_name")+ " " + myRs.getString("first_name"));
 		
