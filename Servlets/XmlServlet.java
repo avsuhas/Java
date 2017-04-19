5bb17a3e1b172ae2b527e080ad0ae69a5223d6c8
Suhas V Arudi <avsuhas@gmail.com>
2017-04-18 21:57:40 -0400

    Update XmlServlet.java

--- Servlets/XmlServlet.java
+++ Servlets/XmlServlet.java
@@ -13,15 +13,41 @@ public class XmlServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String userName = request.getParameter("userName");
-		out.println("Hello from the get method"+ userName);
+		String firstName = request.getParameter("firstName");
+		out.println("Hello from the get method"+ firstName);
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
-		String userName = request.getParameter("userName");
-		out.println("Hello from Post Method"+ userName);
+		String first_name = request.getParameter("first_name");
+		String last_name = request.getParameter("last_name");
+		out.println("Hello from Post Method  "+ first_name +" "+last_name+"\n");
+
+		String Sex = request.getParameter("Sex");
+		out.println("you are a "+Sex);
+		
+		String Address = request.getParameter("Address");
+		out.println("Home Address is:  "+Address);
+		out.println();
+		
+		String City = request.getParameter("City");
+		out.println(City);
+		out.println();
+		
+		String Country = request.getParameter("Country");
+		out.println(Country);
+		out.println();
+		
+		String[] Hobbies = request.getParameterValues("Hobbies");
+		out.println("Your Hobbies are "+ Hobbies.length );
+		for(int i=0; i<Hobbies.length; i++){
+			out.println(Hobbies[i]);
+			
+		
+		}
+		
+	
 	}
 	
 }
