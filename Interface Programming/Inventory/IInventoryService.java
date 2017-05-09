593cee22d9deb229e3f09ef1c1fce55e9c580501
Suhas V Arudi <avsuhas@gmail.com>
2017-05-09 13:45:34 -0400

    Add files via upload

---
+++ Interface Programming/Inventory/IInventoryService.java
@@ -0,0 +1,11 @@
+package com.samson.sakilaproject.service;
+
+import java.util.List;
+
+import com.samson.sakilaproject.bo.InventoryBo;
+
+public interface IInventoryService {
+	
+	public List <InventoryBo> findAll();
+
+}
