9ce9be1cc9878acad1afe01bf1bf29f91063d2fd
Suhas V Arudi <avsuhas@gmail.com>
2017-06-07 11:38:44 -0400

    Add files via upload

---
+++ Spring Udemy/Annotations/TennisCoach.java
@@ -0,0 +1,13 @@
+package com.samson;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "practice backhand volley";
+	}
+
+}
